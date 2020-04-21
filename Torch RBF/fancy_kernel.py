6246f4fe027dff1c714ed45d659cff28253ada0e
Sarah Roden <soumya1910@gmail.com>
2020-04-20 22:29:13 -0400

    Update fancy_kernel.py

--- Torch RBF/fancy_kernel.py
+++ Torch RBF/fancy_kernel.py
@@ -1,34 +1,38 @@
 from torch_rbf import *
+import torch.nn as nn
+import torch.nn.functional as F
 
-class FancyKernel:
+rbf_theta=0.05
+
+class FancyKernel(nn.Module):
 
   def __init__(self,sigma_w,hidden_dim):
-    self.K_theta=
+    self.K_theta=RBF(hidden_dim,1,gaussian(1/(2*(rbf_theta**2))))
     self.sigma_w=sigma_w
-    self.M=
+    self.M=nn.Linear(hidden_dim,2)
     self.optimizer= optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
   def transform_inputs(x):
     ''''
     x.dim = (n,2)
     M.dim = (2,hidden_dim)
     ''''
-    return torch.matmul(x,self.M).data.numpy
+    return nn.Linear(x).data.numpy
   def calc_likelihood(self,x,y):
-    ##CALC LIKELIHOOD IN LOG PROB FORM##
-    return -1 *likelihood #in log probability form
+    K_val=self.K_theta(x) + self.sigma_w * torch.eye(x.size()[0])
+    part1= 0.5 * torch.matmul(torch.matmmul(y,torch.inverse(K_val),y) 
+    part2= 0.5 * torch.log(torch.abs(K_val))
+    return part1 + part2
   def forward(self,data):
     '''
     data is [x,y]
     '''
-    x=transform_inputs(data[0])
+    x=transform_inputs(data[0]) 
     y=data[1]
-    sum=0 #CONVERT TO PYTORCH TENSOR?
-    for i in range(x.size()[0]):
-      sum+=self.calc_likelihood(x[i,:],y[i,:])
-    return sum
+    return self.calc_likelihood(x,y)
+
   def train(self,data):
     for i in range(10):
-      loss=self.forward(data)
+      loss=self.forward(data) #MAKE THIS LIST OF TENSORS
       print(loss.data)
       loss.backward()
       optimizer.step()
